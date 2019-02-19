a1808cde9da99eaff3102182bcc971427ff02f79
abrighton <78px3WT5s>
2019-02-19 15:51:05 +0100

    Still have issues with push/pop

--- csw-event-monitor-client/src/main/scala/csw/eventmon/client/ChartComponent.scala
+++ csw-event-monitor-client/src/main/scala/csw/eventmon/client/ChartComponent.scala
@@ -10,8 +10,8 @@ import csw.params.events.SystemEvent
 case class ChartComponent(eventStreamInfo: P[EventStreamInfo]) extends Component[NoEmit] {
 
   println("XXX ChartComponent constructor")
-  private val maybeDiv = State[Option[Element]](None)
-  private val maybeChart = State[Option[Chart]](None)
+  private var canvasMap  = Map[String, Element]()
+  private var chartMap   = Map[String, Chart]()
   private val maybeEvent = State[Option[SystemEvent]](None)
 
   private def makeId(es: EventSelection): String = {
@@ -21,9 +21,8 @@ case class ChartComponent(eventStreamInfo: P[EventStreamInfo]) extends Component
   private def receiveEvents(get: Get): Unit = {
     get(eventStreamInfo).eventStream.onNext = {
       case event: SystemEvent =>
-//        println(s"XXX Received system event: $event")
-        if (get(maybeChart).nonEmpty)
-          updateChart(get, event)
+        println(s"XXX Received event $event")
+        updateChart(get, event)
         maybeEvent.set(Some(event))
 
       case _ =>
@@ -35,6 +34,7 @@ case class ChartComponent(eventStreamInfo: P[EventStreamInfo]) extends Component
     val options   = ChartOptions()
     val config    = ChartConfiguration("line", chartData, options)
     val chart     = new Chart(id, config)
+
     println(s"XXX chart elem for $id = $chart")
     chart
   }
@@ -44,50 +44,37 @@ case class ChartComponent(eventStreamInfo: P[EventStreamInfo]) extends Component
   }
 
   private def updateChart(get: Get, event: SystemEvent): Unit = {
-    val info = get(eventStreamInfo).eventSelection
-    val maybeParam = if (info.maybeName.nonEmpty) {
-      event.paramSet.find(_.keyName == info.maybeName.get)
-    } else {
-      event.paramSet.find(p => isNumericKey(p.keyType))
-    }
-    if (maybeParam.nonEmpty) {
-      val eventValue = maybeParam.map(_.head.toString.toDouble)
-      val chart = get(maybeChart).get
-      eventValue.foreach(value => Chart.addData(chart, event.eventTime.toString, value))
-    }
-  }
-
-  override def componentWillUnmount(get : Get) : Unit = {
     val id = makeId(get(eventStreamInfo).eventSelection)
-    println(s"XXX ChartComponent will unmount: $id")
+    if (chartMap.contains(id)) {
+      val info = get(eventStreamInfo).eventSelection
+      val maybeParam = if (info.maybeName.nonEmpty) {
+        event.paramSet.find(_.keyName == info.maybeName.get)
+      } else {
+        event.paramSet.find(p => isNumericKey(p.keyType))
+      }
+      if (maybeParam.nonEmpty) {
+        val eventValue = maybeParam.map(_.head.toString.toDouble)
+        eventValue.foreach(value => Chart.addData(chartMap(id), event.eventTime.toString, value))
+      }
+    }
   }
-
 
   override def componentWillRender(get: Get): Unit = {
     // Note: The div has to be added to the dom first, since the chart will look for its id
     val id = makeId(get(eventStreamInfo).eventSelection)
-    println(s"XXX ChartComponent will render: $id")
-    if (get(maybeDiv).isEmpty) {
-      println(s"XXX Create div for $id")
-      val canvas = E.canvas(A.id(id), A.width("400"), A.height("50"))
-      val div    = E.div(A.className("row"), canvas).withRef { _ =>
-        if (get(maybeChart).isEmpty) {
-          println(s"XXX calling makeChart with $id")
-          val chart = makeChart(id, get(eventStreamInfo).eventSelection)
-          maybeChart.set(Some(chart))
-          receiveEvents(get)
-        }
-      }
-      maybeDiv.set(Some(div))
-    } else {
-//      if (get(maybeChart).isEmpty) {
-//        val chart = makeChart(id, get(eventStreamInfo).eventSelection)
-//        maybeChart.set(Some(chart))
-//      }
+    println(s"XXX componentWillRender: $id")
+    if (!canvasMap.contains(id)) {
+      println(s"XXX ChartComponent will render: $id")
+      canvasMap = canvasMap + (id -> E.canvas(A.id(id), A.width("400"), A.height("50")))
+      receiveEvents(get)
+    } else if (!chartMap.contains(id)) {
+      println(s"XXX calling makeChart with $id")
+      chartMap = chartMap + (id -> makeChart(id, get(eventStreamInfo).eventSelection))
     }
   }
 
   override def render(get: Get): Element = {
-    get(maybeDiv).get
+    val id = makeId(get(eventStreamInfo).eventSelection)
+    canvasMap(id)
   }
 }
--- csw-event-monitor-client/src/main/scala/csw/eventmon/client/StripChart.scala
+++ csw-event-monitor-client/src/main/scala/csw/eventmon/client/StripChart.scala
@@ -6,29 +6,15 @@ import csw.eventmon.client.MainComponent.EventStreamInfo
 
 case class StripChart(eventStreams: P[List[EventStreamInfo]]) extends Component[NoEmit] {
 
-//  private val chartMap = State[Map[EventSelection, ConstructorData[NoEmit]]](Map.empty)
-  println("XXX StripChart constructor")
-
-  // Make sure there is a div for each chart
-//  override def componentWillRender(get: Get): Unit = {
-//    get(eventStreams).foreach { info =>
-//      if (!get(chartMap).contains(info.eventSelection)) {
-//        println(s"XXX StripChart create chart for ${info.eventSelection.subsystem}")
-//        val chart = Component(ChartComponent, info)
-//        chartMap.modify(_ + (info.eventSelection -> chart))
-//      }
-//    }
-//  }
+  private def makeId(es: EventSelection): String = {
+    s"${es.subsystem}-${es.maybeComponent.getOrElse("")}-${es.maybeName.getOrElse("")}"
+  }
 
   override def render(get: Get): Element = {
-//    println(s"XXX StripChart render: eventStreams = ${get(eventStreams)}, chartMap keys = ${get(chartMap).keySet}")
-//    val charts = get(eventStreams).map { info =>
-//      get(chartMap)(info.eventSelection)
-//    }
-
     val charts = get(eventStreams).map { info =>
-        println(s"XXX StripChart create chart for ${info.eventSelection.subsystem}")
-        Component(ChartComponent, info)
+      val id = makeId(info.eventSelection)
+      println(s"XXX StripChart create chart for $id")
+      E.div(A.className("row"), E.p(Text(id)), Component(ChartComponent, info))
     }
 
     println(s"XXX num charts: ${charts.size}")
