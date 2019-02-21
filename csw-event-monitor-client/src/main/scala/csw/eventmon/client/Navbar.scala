3e21bc287779b485d661575b1225888f19b5d992
abrighton <78px3WT5s>
2019-02-21 22:51:46 +0100

    Added navbar, worked on tooltips, still need to implement save/load and add css

---
+++ csw-event-monitor-client/src/main/scala/csw/eventmon/client/Navbar.scala
@@ -0,0 +1,62 @@
+package csw.eventmon.client
+import com.github.ahnfelt.react4s._
+import Navbar._
+
+object Navbar {
+  // The navbar emits commands that are handled in the main component
+  sealed trait NavbarCommand
+  case class AddEventSelection(eventSelection: EventSelection) extends NavbarCommand
+
+  val saveDropdownId = "saveDropdown"
+  val loadDropdownId = "loadDropdown"
+}
+
+case class Navbar() extends Component[NavbarCommand] {
+
+  override def render(get: Get): Node = {
+    val navbar = E.nav(
+      E.div(
+        A.className("nav-wrapper teal lighten-2"),
+        E.a(A.href("#!"), A.className("brand-logo"), Text("CSW Event Monitor")),
+        // List of items in the navbar
+        E.ul(
+          A.className("right"),
+          // Add Event
+          Component(EventSelectorComponent).withHandler(es => emit(AddEventSelection(es))),
+          // Save
+          E.li(
+            E.a(A.className("dropdown-trigger"),
+                A.href("#!"),
+                Attribute("data-target", saveDropdownId),
+                Text("Save"),
+                E.i(A.className("arrow_drop_down")))
+          ),
+          // Load
+          E.li(
+            E.a(A.className("dropdown-trigger"),
+              A.href("#!"),
+              Attribute("data-target", loadDropdownId),
+              Text("Load"),
+              E.i(A.className("arrow_drop_down")))
+          )
+        )
+      )
+    )
+
+    val saveDropdown = E.ul(
+      A.id(saveDropdownId),
+      A.className("dropdown-content"),
+      E.li(E.a(A.href("#!"), Text("Save to File"))),
+      E.li(E.a(A.href("#!"), Text("Save to Config Service"))),
+    )
+
+    val loadDropdown = E.ul(
+      A.id(loadDropdownId),
+      A.className("dropdown-content"),
+      E.li(E.a(A.href("#!"), Text("Load to File"))),
+      E.li(E.a(A.href("#!"), Text("Load to Config Service"))),
+    )
+
+    Fragment(saveDropdown, loadDropdown, navbar)
+  }
+}
